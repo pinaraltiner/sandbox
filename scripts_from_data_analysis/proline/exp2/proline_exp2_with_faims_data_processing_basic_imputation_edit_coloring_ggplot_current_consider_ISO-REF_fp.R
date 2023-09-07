90d09a6f5afc3e9569ee716544d13946cca26f33
pinaraltiner <73537017+pinaraltiner@users.noreply.github.com>
2023-09-07 11:26:34 +0200

    1-plotting tp, fp,fn counts added 2- subtitles added

--- scripts_from_data_analysis/proline/exp2/proline_exp2_with_faims_data_processing_basic_imputation_edit_coloring_ggplot_current_consider_ISO-REF_fp.R
+++ scripts_from_data_analysis/proline/exp2/proline_exp2_with_faims_data_processing_basic_imputation_edit_coloring_ggplot_current_consider_ISO-REF_fp.R
@@ -7,7 +7,7 @@ library(tidyr)
 library(ggplot2)
 ###############################################
 source("D:/dev/Pinar/PHD/sandbox/benchmarking_scripts/scripts_from_data_analysis/ggplot/ggplot_functions.R")
-source("D:/dev/Pinar/PHD/sandbox/benchmarking_scripts/scripts_from_data_analysis/roc_curve/roc_curve_generation_proline_edit.R")
+#source("D:/dev/Pinar/PHD/sandbox/benchmarking_scripts/scripts_from_data_analysis/roc_curve/roc_curve_generation_proline_edit.R")
 # Experiment 2
  #file_path <- "D:/dev/Pinar/PHD/wet_lab_experiments/DDA_data_analysis/experiment_2/Proline_data_analysis/exp2_re_injection/"
  #file_name <- "PAL _Phosphopeptides exp2 ( 5 conc 3reps) DDA_230117 with Design_2023-06-07_1003.xlsx"
@@ -191,6 +191,33 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
         slice(which.max(intensity)) %>%
         ungroup()
     
+    ####### ADDITIONAL PLOT TO DISPLAY MISSING and UNEXPECTED PEPTIDES ########
+    df_merge_syn <- barplt_df %>%
+      select(pep_with_pos,sample_ids,intensity, accession) %>% 
+      pivot_wider(names_from = "sample_ids",values_from = "intensity") %>%
+      full_join(pep_list_w_theo_quant_new,by="pep_with_pos") %>% 
+      mutate_at("Pool", ~replace_na(.,"Unexpected")) %>%
+      mutate(Pool= ifelse(is.na(accession),"missing",Pool)) %>%
+      select(pep_with_pos,starts_with(exp_design),Pool) %>%
+      mutate(soft_name=software_name,ion_mobility=acquisiton_type)
+    
+    p11 <- gg_barplt_id_pep_count(data_set = df_merge_syn,
+                                  x_df = df_merge_syn$Pool,
+                                  fill_df = df_merge_syn$Pool,
+                                  ymax = 20000,
+                                  header = "Total number of quantified phospho-site across each sample",
+                                  caption_lab = "NA values are removed.",
+                                  x_lab = "Sample id",
+                                  fill_lab =  "Sample id",
+                                  y_lab = "Number of identified peptides",
+                                  subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
+    
+    write.table(df_merge_syn,file = paste0(file_path,"Count_of_missing_unexpected_correct_phospho-sites_",
+                                           software_name,"_Experiment",exp_id,".txt"),
+                sep = "\t",row.names = F)
+    #############################################################################
+    
+    
     ## SAME STRATEGIES ABOVE (3rd) WAS APPLIED TO BACKGROUND AS WELL
     barplt_df_ecoli <- filtered_abundances_ecoli %>% 
         select(sequence,modifications, sequence, starts_with(exp_design),accession) %>%
@@ -240,7 +267,7 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
                                  x_lab = "Sample id",
                                  fill_lab =  "Sample id",
                                  y_lab = "Number of identified peptides",
-                                 subtitle_txt = "")
+                                 subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
     
     
     p2 <- gg_barplt_id_pep_count(data_set = barplt_df_ecoli,
@@ -252,7 +279,7 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
                                  x_lab = "Sample id",
                                  fill_lab =  "Sample id",
                                  y_lab = "Number of identified peptides",
-                                 subtitle_txt = "")
+                                 subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
     
     ## THIS RESHAPING IS ONLY FOR ELIMINATION OF MULTIPLE PHOSPHO-SITES and ECOLI PEPTIDES
     ## ELIMINATION STEP IS NOT NECESSARY FOR ECOLI, 1st STRATEGY can be used only (this will decrease lines of code)
@@ -354,12 +381,12 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
                      x_lab = "log10(intensities)",
                      color_lab= "",
                      fill_lab = "species",
-                     subtitle_txt = "")
+                     subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
     
     
-    abundances_for_before_impt <- barplt_df_wide %>%
-        bind_rows(barplt_df_ecoli_wide) %>% select(exp_design)
-    
+    # abundances_for_before_impt <- barplt_df_wide %>%
+    #     bind_rows(barplt_df_ecoli_wide) %>% select(exp_design)
+    # 
     # Calculate 1 percent quantile of each sample
     impute_values <- apply(abundances_for_impute, 2 , quantile , probs = 0.01 , na.rm = TRUE )
     
@@ -478,7 +505,7 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
                      x_lab = "log10(values)",
                      color_lab= "",
                      fill_lab = "Sample Names",
-                     subtitle_txt = "")
+                     subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
     
     # px <- gg_raincloud(data_set = df_mean_ab_after_impt,
     #                    x_df = df_mean_ab_after_impt$Mean_abundance,
@@ -500,7 +527,7 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
                      x_lab = "log10(values)",
                      color_lab= "",
                      fill_lab = "Sample Names",
-                     subtitle_txt = "")
+                     subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
     # 
     
     ### BOX-PLOT: Experimental Quantity Ratio of Phospho Peptides  
@@ -513,7 +540,7 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
                               x_lab="Sample Names",
                               y_lab="Abundance Ratios",
                               fill_lab = "Sample Names",
-                              subtitle_txt = "")
+                              subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
     
     
     ### HALF-BOX-PLOT & HALF-SCATTER-PLOT: Experimental Quantity Ratio of Synthetic Peptides  
@@ -527,7 +554,7 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
                                    x_lab="Sample Names",
                                    y_lab="Abundance Ratios",
                                    fill_lab = "Sample Names",
-                                   subtitle_txt = "")
+                                   subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
     
     ### VIOLIN-PLOT: Experimental Quantity Ratio of Synthetic Peptides   
     
@@ -541,7 +568,7 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
                               y_lab="Abundance Ratios",
                               fill_lab = "Sample Names",
                               trim=TRUE,
-                              subtitle_txt = "")
+                              subtitle_txt = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name))
     
     #   selected_cols <- colnames(exp2_FAIMS_DDA_proline[c(1:4,6,28)])
     # #selected_cols <- colnames(exp2_noFAIMS_DDA_proline[c(1:4,15)])
@@ -798,7 +825,8 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
             unite(Pool_new, Pool.x, isomericity,sep = "_",remove = FALSE) %>%
             unite('new_col_coloring',Pool_new,ratio,sep = "_",remove = FALSE) %>%
             mutate(new_col_coloring = if_else(grepl("ISO-REF", new_col_coloring), "ISO-REF", new_col_coloring)) %>%
-            mutate(new_col_coloring = if_else(grepl("unexpected", new_col_coloring), "unexpected", new_col_coloring))
+            mutate(new_col_coloring = if_else(grepl("unexpected", new_col_coloring), "unexpected", new_col_coloring)) %>%
+            rename(pep_with_pos=pep_with_pos.x) %>% rename(Pool=Pool.x)
         
     }else{
         print("Statistical test could not be assessed. Check the input files!")
@@ -891,7 +919,7 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
               axis.title = element_text(size = 15),
               axis.text.y = element_text(size = 15),
               plot.subtitle = element_text(size = 15)) +
-        labs( y= "-log10(p values)", x="log2(fold change)",title = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name), subtitle = "Limma was used \n",subtitle) +
+        labs( y= "-log10(p values)", x="log2(fold change)",title = paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name), subtitle = paste("Limma was used \n",subtitle)) +
         geom_vline(data = actual_ratio_col, aes(xintercept = log2(actual_ratio_val), show.legend = FALSE),color=c("#CC79A7","#E69F00","#56B4E9","#009E73"),size=1) +
         geom_hline(yintercept = -log10(fdr_threshold), linetype = "dashed", color = "red",size=1) + 
         geom_label(data = point_count_y_axis, aes(x = log2(actual_ratio_val), y = y_pos,fill=new_col_coloring, label = n),size=6, colour="white",show.legend = FALSE) 
@@ -903,10 +931,10 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
     
     #### ROC Analysis
     df_roc <- merge_stat_df_final %>%
-        select(pep_with_pos.x, Pool.x,P.Value)
+        select(pep_with_pos, Pool,P.Value)
     
-    df_roc$variant <- ifelse(df_roc$Pool.x == "Others", TRUE, FALSE)
-    df_roc$non_var <- ifelse(df_roc$Pool.x == "ISO-REF", TRUE, FALSE)
+    df_roc$variant <- ifelse(df_roc$Pool == "Others", TRUE, FALSE)
+    df_roc$non_var <- ifelse(df_roc$Pool == "ISO-REF", TRUE, FALSE)
     
     library(pROC)
     # Calculate ROC curve for raw p-values
@@ -929,15 +957,15 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
     
     p10 <- roc_plot_df %>% group_by(Pool_type) %>% 
         ggplot( aes(y=as.numeric(sensitivity), x = as.numeric(specificity), color=Pool_type)) +
-        geom_point() +  scale_x_reverse() + theme_bw() +
-        theme(legend.text = element_text(size = 15),
-              axis.title.x = element_text(size = 15),
-              axis.title.y = element_text(size = 15),
-              plot.title = element_text(size = 30),
-              legend.title = element_text(size = 15),
-              axis.text.x = element_text(size = 15),
-              axis.title = element_text(size = 15),
-              axis.text.y = element_text(size = 15)) +
+        geom_path(size=1.5) +  scale_x_reverse() + theme_bw() +
+        theme(legend.text = element_text(size = 20),
+              axis.title.x = element_text(size = 20),
+              axis.title.y = element_text(size = 20),
+              plot.title = element_text(size = 25),
+              legend.title = element_text(size = 20),
+              axis.text.x = element_text(size = 20),
+              axis.title = element_text(size = 20),
+              axis.text.y = element_text(size = 20)) +
         scale_color_brewer(palette = "Dark2") +
         labs(y="True Positive Rate \n (Sensitivity)", x="False Positive Rate \n (Specificity)",
              title =  paste("Experiment - ", exp_id, acquisiton_type, " data processed by ", software_name), 
@@ -945,8 +973,8 @@ final_proline_pep_quant_analysis_syn <- function(file_path,
     
     write.table(roc_plot_df, file = paste0(file_path,"Roc_analysis_",exp_id,"_",software_name,"_",".txt"),sep = "\t",row.names = F) #acquisiton_type ## IT WAS TOO LONG-> GIVES AN ERROR
     
-    sapply(1:10,function(x) ggsave(filename = paste0("p",x,".tiff"),
-                                   width = 50, height = 40, 
+    sapply(1:11,function(x) ggsave(filename = paste0("p",x,".tiff"),
+                                   width = 50, height = 45, 
                                    path = paste0(file_path,"/outputs_with_new_script/"),
                                    units = "cm",
                                    get(paste0("p",x)),
