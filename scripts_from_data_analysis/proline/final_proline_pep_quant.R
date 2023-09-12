73c3c63621eed1a8c8f07c5861ef87e81c059536
pinaraltiner <73537017+pinaraltiner@users.noreply.github.com>
2023-09-12 23:59:04 +0200

    re-writing file paths

--- scripts_from_data_analysis/proline/final_proline_pep_quant.R
+++ scripts_from_data_analysis/proline/final_proline_pep_quant.R
@@ -5,45 +5,111 @@ library(data.table)
 library(openxlsx)
 library(tidyr)
 
-# Experiment 2
-file_path_exp2 <- "D:/dev/Desktop_copy/PHD/wet_lab_experiments/DDA_data_analysis/experiment_2/Proline_data_analysis/"
-file_name_exp2 <- "PAL - 2023 01 04 - Phosphopeptides exp2 ( 5 conc 3 reps) DDA_2023-01-06_1032.xlsx"
-selected_spcies = "_HUMAN"
+#
+source("D:/dev/Pinar/PHD/sandbox/benchmarking_scripts/scripts_from_data_analysis/proline/exp2/proline_exp2_with_faims_data_processing_basic_imputation_edit_coloring_ggplot_current_consider_ISO-REF_fp.R")
+
+source("D:/dev/Pinar/PHD/sandbox/benchmarking_scripts/scripts_from_data_analysis/proline/exp3/merging_quant_pep_and_t_cells_theme_edit_29032023.R")
+
+file_paths <- c(paste0("D:/dev/Pinar/PHD/wet_lab_experiments/DDA_data_analysis/experiment_3/Proline_data_analysis/", c("quant_pep_06022023/","/with_FAIMS/")),
+                "D:/dev/Pinar/PHD/wet_lab_experiments/DDA_data_analysis/experiment_3/Proline_timsdata/",
+                paste0("D:/dev/Pinar/PHD/wet_lab_experiments/DDA_data_analysis/experiment_2/Proline_data_analysis/", c("exp2_re_injection/","with_FAIMS/")),
+                "D:/dev/Pinar/PHD/wet_lab_experiments/DDA_data_analysis/experiment_2/Proline_timsdata/")
+
+
+file_names <- c("PAL _T_cell_Exp3_( 5 conc 3reps)_NoFAIMS_DDA_with_cont_230206_2023-02-07_0947.xlsx",
+                "PAL _T_cell_Exp3_( 5 conc 3reps)_withFAIMS_DDA_25052023_noDesign_2023-07-10_1134.xlsx",
+                "PAL TimsTOF_data_conversion_all_exp_06062023 E3 corrected_2023-06-28_1522.xlsx",
+                "PAL _Phosphopeptides exp2 ( 5 conc 3reps) DDA_230117 with Design_2023-06-07_1003.xlsx",
+                "PAL _Exp2_( 5 conc 3reps)_withFAIMS_DDA_25052023_noDesign - correct_2023-06-13_1514.xlsx",
+                "PAL TimsTOF_data_conversion_all_exp_06062023 E2 corrected_2023-06-28_1522.xlsx")
+
+
+
+
+acquisiton_types <- c("DDA Exploris no FAIMS","DDA Exploris with FAIMS","DDA TIMS-TOF",
+                      "DDA Exploris no FAIMS","DDA Exploris with FAIMS","DDA TIMS-TOF")
+#test_type <- c("t.test","wilcoxon","limma")
+#software_name <- c("Proline", "MaxQuant", "PD")
+#exp_id <- 1:3
+experimental_design <- c("E3_A1_R1","E3_A1_R2",
+                         "E3_A1_R3",
+                         "E3_A2_R1",
+                         "E3_A2_R2",
+                         "E3_A2_R3",
+                         "E3_A3_R1",
+                         "E3_A3_R2",
+                         "E3_A3_R3",
+                         "E3_A4_R1",
+                         "E3_A4_R2",
+                         "E3_A4_R3",
+                         "E3_A5_R1",
+                         "E3_A5_R2",
+                         "E3_A5_R3")
+
+#suppressPackageStartupMessages() = To Eliminate Package Startup Messages
+
+for ( i in 1:3){
+  final_proline_pep_quant_analysis_bio(file_path =file_paths[i],
+                                       file_name = file_names[i],
+                                       sheet_name = "Best PSM from protein sets",
+                                       #sheet_theo_name <- "ISO-ref and OTHER with FC",
+                                       selected_spcies = "_MOUSE",
+                                       exp_design = experimental_design,
+                                       acquisiton_type= acquisiton_types[i],
+                                       num_reps = 3,
+                                       exp_id=3,
+                                       software_name = "Proline",
+                                       test_type="limma",
+                                       subtitle=file_names[i])
+}
+
+
 
 # Correct syn. peptide list for Experiment 2
-theo_file_path <- "D:/dev/Desktop_copy/PHD/wet_lab_experiments/Eyers_syn_peptides_experiment/"
-theo_file_name <- "Synthetic peptides list_theo_conc_added_080122.xlsx"
-sheet_theo_name <- "ISO-ref and OTHER with FC"
-
-# Experiment 3
-file_path_exp3 <- "D:/dev/Desktop_copy/PHD/wet_lab_experiments/DDA_data_analysis/experiment_3/Proline_data_analysis/quant_pep_06022023/"
-file_name_exp3 <- "PAL _Tcell_Exp3_( 5 conc 3reps)_NoFAIMS_DDA_with_cont_230206.xlsx"
-selected_spcies = "_MOUSE"
 
 
-# Common constant objects
-sample_size <- 5
-sheet_name <- "Quantified peptide ions"
+# Experiment 2
+
+experiment_name <- c("E2_A1_R1","E2_A1_R2",
+                "E2_A1_R3",
+                "E2_A2_R1",
+                "E2_A2_R2",
+                "E2_A2_R3",
+                "E2_A3_R1",
+                "E2_A3_R2",
+                "E2_A3_R3",
+                "E2_A4_R1",
+                "E2_A4_R2",
+                "E2_A4_R3",
+                "E2_A5_R1",
+                "E2_A5_R2",
+                "E2_A5_R3")
 
 
-#source("D:/dev/Desktop_copy/PHD/data_analysis/scripts/ggplot_functions.R")
-#source("D:/dev/Desktop_copy/PHD/data_analysis/scripts/with_tidyr_merging_quant_pep_and_pep_list_w_theo_quant_24032023.R")
+for (i in 4:length(file_paths)){
+    final_proline_pep_quant_analysis_syn(file_path =file_paths[i],
+                                         file_name = file_names[i],
+                                         sheet_name = "Best PSM from protein sets",
+                                         theo_file_path= "D:/dev/Pinar/PHD/wet_lab_experiments/Eyers_syn_peptides_experiment/",
+                                         theo_file_name="Synthetic peptides list_theo_conc_corrected_pool_id_iso_count_final.xlsx",
+                                         sheet_theo_name = "ISO-ref and OTHER with FC",
+                                         acquisiton_type= acquisiton_types[i],
+                                         software_name = "Proline",
+                                         test_type="limma",
+                                         selected_spcies = "_HUMAN",
+                                         background_species = "ECOLI",
+                                         exp_id=2,
+                                         num_reps=3,
+                                         exp_design=experiment_name,
+                                         fdr_threshold=0.05,
+                                         actual_ratio=c(2,10,20,100),
+                                         subtitle=file_names[i])
+}
 
-source("D:/dev/Desktop_copy/PHD/data_analysis/scripts/merging_quant_pep_and_t_cells_theme_edit_29032023.R")
-final_proline_pep_quant_analysis_syn(file_path =file_path_exp3,
-                                 file_name = file_name_exp3,
-                                 sheet_name = sheet_name,
-                                 theo_file_path = theo_file_path,
-                                 theo_file_name = theo_file_name,
-                                 sheet_theo_name = sheet_theo_name,
-                                 selected_spcies = "_HUMAN",
-                                 sample_size = 5)
 
-final_proline_pep_quant_analysis_bio(file_path =file_path_exp3,
-                                 file_name = file_name_exp3,
-                                 sheet_name = sheet_name,
-                                 theo_file_path = theo_file_path,
-                                 theo_file_name = theo_file_name,
-                                 sheet_theo_name = sheet_theo_name,
-                                 selected_spcies = "_MOUSE",
-                                 sample_size = 5)
+
+
+
+
+
+
